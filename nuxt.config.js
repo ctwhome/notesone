2b7969deb30014863cee61e08961d7973b596d7d
Jesús García <ctw@ctwhome.com>
2021-07-31 12:58:22 +0200

    minor: add web title and favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-basis',
+    title: 'NotesOne',
+    description: 'The OneNote alternative.',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
